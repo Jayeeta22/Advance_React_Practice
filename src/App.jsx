1a1b62465cbc0076b6e99ec5fc4f76a454cd74aa
Jayeeta22 <jayeeta97.riya@gmail.com>
2025-04-07 19:08:34 +0530

    project setup with vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 class="text-3xl font-bold underline">
+    Project setup with vite and tailwind css!
+  </h1>
+        
+    </>
+  )
+}
+
+export default App
